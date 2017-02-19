532dc7fc2f542d2eb4c353492e5738e6479a599f
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-19 20:17:18 +0100

    initial setup

---
+++ src/Container.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Container extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                
+            </div>
+        );
+    }
+}
+
+export default Container;
