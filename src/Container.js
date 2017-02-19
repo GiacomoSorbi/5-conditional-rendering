5c50d66345fb00799338f63aedab45e4c594ce22
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-19 20:31:15 +0100

    initial solution

--- src/Container.js
+++ src/Container.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import If from './If.js';
 import './App.css';
 
 class Container extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
     render() {
         return (
             <div className="container">
-                
+                <If condition={true}>
+                    <p>Amazing! I have rendered!  Remember:  props.condition can be any Javascript expression evaluating to a boolean, such as values from application state.</p>
+                </If>
             </div>
         );
     }
---
+++ src/If.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function If(props) {
+    return (props.condition) && props.children;
+}
+
+export default If;
